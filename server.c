64530d74ed4ab7d697d5e8e1bcf54bd079a207e5
Lukas Holmberg <44261196+luxxen@users.noreply.github.com>
2019-05-18 22:12:42 +0200

    Create server.c

---
+++ server.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+
+#include <fcntl.h>
+#include <unistd.h>
+int main(){
+
+	char msg[256];
+
+	int serSock;
+	serSock = socket(AF_INET, SOCK_STREAM, 0);
+
+	struct sockaddr_in server_adress;
+	server_adress.sin_family = AF_INET;
+	server_adress.sin_port = htons(9002);
+	server_adress.sin_addr.s_addr = INADDR_ANY;
+
+	bind(serSock, (struct sockaddr*) &server_adress, sizeof(server_adress));
+
+	listen(serSock, 5);
+
+	int client_socket; 
+	client_socket = accept(serSock, NULL,NULL);
+	scanf("%s", msg);
+
+	send(client_socket, msg, sizeof(msg),0);
+
+	close(serSock);
+
+	return 0;
+}
