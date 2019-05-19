1ec681fcbdd1c9b00feff5286ee5b9cbba3acc71
Lukas Holmberg <44261196+luxxen@users.noreply.github.com>
2019-05-19 18:33:09 +0200

    Update server.c

--- server.c
+++ server.c
@@ -3,35 +3,32 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
-
 #include <fcntl.h>
 #include <unistd.h>
 
 
 int main(int argc, char *argv[]){
-
-	char msg[256];
-
-	int serSock;
-	serSock = socket(AF_INET, SOCK_STREAM, 0);
-
-	struct sockaddr_in server_adress;
-	server_adress.sin_family = AF_INET;
-	server_adress.sin_port = htons(9002);
-	server_adress.sin_addr.s_addr = INADDR_ANY;
-
-	bind(serSock, (struct sockaddr*) &server_adress, sizeof(server_adress));
-
-	listen(serSock, 5);
-
-	int client_socket; 
-	client_socket = accept(serSock, NULL,NULL);
-	while(1){
-	scanf("%s", msg);	
-	send(client_socket, msg, sizeof(msg),0);
-	}
-
-	close(serSock);
-
-	return 0;
+    
+    int serSock;
+    serSock = socket(AF_INET, SOCK_STREAM, 0);
+    
+    struct sockaddr_in server_adress;
+    server_adress.sin_family = AF_INET;
+    server_adress.sin_addr.s_addr = INADDR_ANY;
+    server_adress.sin_port = 3002;
+    
+    bind(serSock, (struct sockaddr*) &server_adress, sizeof(server_adress));
+    
+    listen(serSock, 5);
+    char msg[256];
+    int client_socket;
+    client_socket = accept(serSock, NULL,NULL);
+    while(1){
+        scanf("%s", msg);
+        send(client_socket, msg, sizeof(msg),0);
+    }
+    
+    close(serSock);
+    
+    return 0;
 }
