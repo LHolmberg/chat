309fb7ffe5f627501c9bc454e80257f85d3781a0
Lukas Holmberg <44261196+luxxen@users.noreply.github.com>
2019-05-25 21:39:56 +0200

    Update SERVER.c

--- SERVER.c
+++ SERVER.c
@@ -10,15 +10,13 @@
 
 #define maxlength 100
 
-struct sockaddr_in serv;
-int fd;
-int conn;
-char message[100];
-
 int main(int argc, char *argv[]){
-
-	serv.sin_family = AF_INET;
-serv.sin_port = htons(8096);
+	struct sockaddr_in serv;
+	int fd;
+	int conn;
+	char message[100];
+serv.sin_family = AF_INET;
+serv.sin_port = htons(3002);
 serv.sin_addr.s_addr = INADDR_ANY;
 fd = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -30,8 +28,9 @@ while(conn = accept(fd, (struct sockaddr *)NULL, NULL)) {
     int pid;
     if((pid = fork()) == 0) {
         while (recv(conn, message, 100, 0)>0) {
-            printf("Message Received: %s\n", message);
-
+            printf("%s\n", message);
+						message[0] = '\0';
+						memset(&message[0], 0, sizeof(message));
         }
         exit(0);
     }
