b34988feaa2fad3720e8cc00966a6e1cb67bd8d5
Lukas Holmberg <44261196+luxxen@users.noreply.github.com>
2019-05-25 18:53:41 +0200

    Add files via upload

---
+++ SERVER.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <gtk/gtk.h>
+
+#define maxlength 100
+
+struct sockaddr_in serv;
+int fd;
+int conn;
+char message[100];
+
+int main(int argc, char *argv[]){
+
+	serv.sin_family = AF_INET;
+serv.sin_port = htons(8096);
+serv.sin_addr.s_addr = INADDR_ANY;
+fd = socket(AF_INET, SOCK_STREAM, 0);
+
+bind(fd, (struct sockaddr *)&serv, sizeof(serv));
+listen(fd,5);
+
+
+while(conn = accept(fd, (struct sockaddr *)NULL, NULL)) {
+    int pid;
+    if((pid = fork()) == 0) {
+        while (recv(conn, message, 100, 0)>0) {
+            printf("Message Received: %s\n", message);
+
+        }
+        exit(0);
+    }
+}
+}
