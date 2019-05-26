50a15b8590fb7c9ea413d0f459603fd0cd722142
Lukas Holmberg <44261196+luxxen@users.noreply.github.com>
2019-05-26 11:53:58 +0200

    Update SERVER.c

--- SERVER.c
+++ SERVER.c
@@ -13,28 +13,31 @@
 
 
 int main(int argc, char *argv[]){
+
 	struct sockaddr_in serv;
 	int fd;
 	int conn;
 	char message[100];
-serv.sin_family = AF_INET;
-serv.sin_port = htons(3002);
-serv.sin_addr.s_addr = INADDR_ANY;
-fd = socket(AF_INET, SOCK_STREAM, 0);
 
-bind(fd, (struct sockaddr *)&serv, sizeof(serv));
-listen(fd,5);
+	serv.sin_family = AF_INET;
+	serv.sin_port = htons(3002);
+	serv.sin_addr.s_addr = INADDR_ANY;
+	fd = socket(AF_INET, SOCK_STREAM, 0);
 
+	bind(fd, (struct sockaddr *)&serv, sizeof(serv));
+	listen(fd,5);
 
-while(conn = accept(fd, (struct sockaddr *)NULL, NULL)) {
-    int pid;
-    if((pid = fork()) == 0) {
-        while (recv(conn, message, 100, 0)>0) {
-            printf("%s\n", message);
-						message[0] = '\0';
-						memset(&message[0], 0, sizeof(message));
-        }
-        exit(0);
-    }
-}
+	while(conn = accept(fd, (struct sockaddr *)NULL, NULL)) {
+	    int pid;
+	    if((pid = fork()) == 0) {
+	        while (recv(conn, message, 100, 0)>0) {
+	            printf("%s\n", message);
+							message[0] = '\0';
+							memset(&message[0], 0, sizeof(message));
+	        }
+	        exit(0);
+
+	    }
+
+	}
 }
