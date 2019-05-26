9250cde9b52b2f4263bfa917fcd131768b4e5af9
Lukas Holmberg <44261196+luxxen@users.noreply.github.com>
2019-05-26 11:55:04 +0200

    Update CLIENT.c

--- CLIENT.c
+++ CLIENT.c
@@ -56,9 +56,9 @@ int main(int argc, char *argv[]) {
 
 	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-	 gtk_window_set_title (GTK_WINDOW (window), vars.welcomeUsername);
+	gtk_window_set_title (GTK_WINDOW (window), vars.welcomeUsername);
 	grid = gtk_grid_new();
-  gtk_container_add(GTK_CONTAINER(window), grid);
+  	gtk_container_add(GTK_CONTAINER(window), grid);
 
 	entry = gtk_entry_new();
 	g_signal_connect(entry, "activate", G_CALLBACK(print), NULL);
