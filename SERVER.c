72a3eb3af7b27f0f63f50867fbed866f42dcbd53
Lukas Holmberg <44261196+luxxen@users.noreply.github.com>
2019-05-29 00:50:42 +0200

    Update SERVER.c

--- SERVER.c
+++ SERVER.c
@@ -11,7 +11,7 @@
 
 #define maxlength 100
 
-struct sock{
+typedef struct sock{
 	int serverSock;
 	int client;
 	int connection;
@@ -19,25 +19,25 @@ struct sock{
 	struct sockaddr_in server;
 	pthread_t thread1, thread2;
 	int i1,i2;
-    GtkWidget *window, *label;
-}so;
+  GtkWidget *window, *label;
+}sock;
+struct sock so;
 
 void *MessageHandeler(void *arg){
 	while(so.connection = accept(so.client, (struct sockaddr *)NULL, NULL)) {
 		int i;
 		if((i = fork()) == 0) {
-	  		while (recv(so.connection, so.msg, 100, 0)>0) {
-				printf("%s\n", so.msg);
-				so.msg[0] = '\0';
-				memset(&so.msg[0], 0, sizeof(so.msg));
-				}
-			exit(0);
+  		while (recv(so.connection, so.msg, 100, 0)>0) {
+			printf("%s\n", so.msg);
+			so.msg[0] = '\0';
+			memset(&so.msg[0], 0, sizeof(so.msg));
+			}
+		exit(0);
 		}
 	}
 }
 
 void *GraphicsHandeler(void *arg){
-
 	gtk_main();
 }
 
@@ -50,16 +50,16 @@ int main(int argc, char *argv[]){
 	bind(so.client, (struct sockaddr *)&so.server, sizeof(so.server));
 	listen(so.client, 5);
 
-    gtk_init (&argc, &argv);
+	gtk_init (&argc, &argv);
 
-    so.window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title (GTK_WINDOW(so.window), "Chat");
-		gtk_window_set_default_size(GTK_WINDOW(so.window), 500, 200);
+	so.window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title (GTK_WINDOW(so.window), "Chat");
+	gtk_window_set_default_size(GTK_WINDOW(so.window), 500, 200);
 
-    so.label = gtk_label_new ("helloooo");
-    gtk_container_add (GTK_CONTAINER (so.window), so.label);
+	so.label = gtk_label_new ("helloooo");
+	gtk_container_add (GTK_CONTAINER (so.window), so.label);
 
-    gtk_widget_show_all (so.window);
+	gtk_widget_show_all (so.window);
 
 	so.i1 = pthread_create( &so.thread1, NULL, MessageHandeler, (void*) "thread 1");
 	so.i2 = pthread_create( &so.thread2, NULL, GraphicsHandeler, (void*) "thread 2");
